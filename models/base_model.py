42e61fc1cef70622612904f38ab0dc2bdf5209e2
root <root@MOUH-213>
2024-01-18 02:27:43 +0100

    done

--- models/base_model.py
+++ models/base_model.py
@@ -15,22 +15,16 @@ class BaseModel():
     updated_at=Column(DateTime, nullable=True, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
-                if key=='updated_at' or key =='created_at':
-
-                    kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-                    kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-                if hasattr(self,key):
-                    setattr(self, key, value)
-
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if hasattr(self, key):
+                    if key != "__class__":
+                        setattr(self, key, value)
             #del kwargs['__class__']
             #self.__dict__.update(kwargs)
 
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,7 +14,9 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-class DBStorage:
+
+class DBStorage():
+
     """
     Mysql preparation and session
     """
